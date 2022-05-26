57cd87196f3ebf0220e3ff01dd68970285bea40c
unknown <aayusggupta09022003@gmail.com>
2022-05-26 21:03:23 +0530

    Basic U/I done

--- App.py
+++ App.py
@@ -1,5 +1,6 @@
-import pygame
+import pygame, time
 from pygame import MOUSEBUTTONDOWN, mixer
+
 
 pygame.init()
 
@@ -11,21 +12,29 @@ white = (255,255,255)
 grey = (128,128,128)
 green = (0,255,0)
 gold = (212,175,55)
+blue = (0,255,255)
 
 #set the screen/window
 screen = pygame.display.set_mode([width,height])
 pygame.display.set_caption("SICKO MODE")
 
-label_font = pygame.font.Font('WigendaTypewrite.ttf',30)
+label_font = pygame.font.Font('WigendaTypewrite.ttf', 30)
 fps = 60
 beats = 8
 sounds = 6
 boxes=[]
-clicked = [[-1 for i in range(beats)]  for i in range(sounds)] 
+clicked = [[-1 for _ in range(beats)] for _ in range(sounds)]
+activeLength = 0
+activeBeat = 0
+bpm = 240 
+beatChanged = True
+Playing = True
+start = True
 timer = pygame.time.Clock()
 
 
-def drawGrid() :
+def drawGrid(clicked,activeBeat) :
+    global label_font, fps, beats, sounds, boxes, timer
     left_box = pygame.draw.rect(screen,grey,[0,0,200,height-200],5)
     bottom_box = pygame.draw.rect(screen,grey,[0,height-200,width,200],5)
     boxes = []
@@ -48,39 +57,52 @@ def drawGrid() :
 
     for i in range(beats):
         for j in range(sounds):
-            if clicked[j][i] == -1 :
+            if clicked[j][i] == -1:
                 color = grey
-            else :
+            else  :
                 color = green
 
             rect = pygame.draw.rect(screen,color,[i*(width-200)//beats+205,(j*100)+5,(width-200)//beats-10,((height-200)//sounds)],0,3)
 
-            pygame.draw.rect(screen,black,[i*(width-200)//beats+200,(j*100),(width-200)//beats,((height-200)//sounds)],3,3)
             pygame.draw.rect(screen,gold,[i*(width-200)//beats+200,(j*100),(width-200)//beats,((height-200)//sounds)],3,3)
+            pygame.draw.rect(screen,black,[i*(width-200)//beats+200,(j*100),(width-200)//beats,((height-200)//sounds)],1,1)
             
             boxes.append((rect,(i,j)))
+        
+        
+        active = pygame.draw.rect(screen, blue,[activeBeat * ((width - 200) // beats) + 200, 0, ((width - 200) // beats), sounds * 100],3,3)
 
     return boxes
 
-
-
-start = True
 
 while start :
     timer.tick(fps)
     screen.fill(black)
 
-    boxes = drawGrid()  
+    boxes = drawGrid(clicked,activeBeat)  
 
-    for event in pygame.event.get():
+    for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
             start  = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             for i in range(len(boxes)):
-                if boxes[i][0].colliderect(event.pos) :
+                if boxes[i][0].collidepoint(event.pos) :
                     coords = boxes[i][1]
                     clicked[coords[1]][coords[0]] *= -1
 
+    beatLength = 3600//bpm
+
+    if Playing :
+        if activeLength < beatLength:
+            activeLength += 1
+        else :
+            activeLength = 0
+            if activeBeat < beats-1 :
+                activeLength += 1 
+                beatChanged = True
+            else:
+                activeBeat = 0
+                beatChanged = True
 
     pygame.display.flip()
 
